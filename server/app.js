f5bee8da5b2c97dc6e2950cbdc5908e9ac691ab1
Christophe Calvet <onlyatest@mytest.com>
2024-09-04 15:58:48 +0200

    phase 3 done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
 
+//For phase 1
+app.use(express.json());
+require('express-async-errors');
+
+const path = require('path');
+app.use("/static",express.static(path.join(__dirname, '/assets')))
+
+
+//Phase2 Logger Middleware
+const logMiddleware = (req, res, next) => {
+  console.log(req.method, req.url);
+    res.on('finish', () => {
+      console.log(res.statusCode);
+    });
+  next();
+};
+app.use(logMiddleware)
+
+
+
+//PHHASE 3
+const dogsRouter = require("./routes/dogs.js")
+app.use("/dogs",dogsRouter)
+
+
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -18,6 +44,16 @@ app.post('/test-json', (req, res, next) => {
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
 });
+
+//Phase 2 resource not found
+app.use((req, res, next)=>{
+  const myError = new Error("The requested resource couldn't be found.")
+  myError.statusCode = 404;
+  throw myError;
+})
+
+
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
